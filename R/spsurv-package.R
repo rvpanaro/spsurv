f417cbe37df86f60695fecceb2c58ba121130d64
RVP07 <35786367+rvpanaro@users.noreply.github.com>
2019-04-03 14:44:50 -0300

    First Commit

---
+++ R/spsurv-package.R
@@ -0,0 +1,18 @@
+#' The 'spsurv' package.
+#'
+#' @description Semiparametric Survival Analysis
+#'
+#' @docType package
+#' @name spsurv-package
+#' @aliases spsurv
+#' @useDynLib spsurv, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @import rstantools
+#' @importFrom rstan sampling
+#' @importFrom survival Surv
+#'
+#' @references
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. http://mc-stan.org
+#'
+NULL
