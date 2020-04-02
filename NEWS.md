c37edecfd471892f15fc61cc76ac8a2c5e139bb3
renatovp <renatovp@id.uff.br>
2020-04-02 09:30:48 -0300

    add actions+ pckgdown + hex

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# spsurv 1.0.0
+
+* This is the first release of pkgdown.
--- inst/dev.R
+++ inst/dev.R
@@ -5,6 +5,8 @@ devtools::document()
 # devtools::install()
 devtools::install(quick=TRUE)
 
+usethis::use_build_ignore("_pkgdown.yml")
+usethis::use_build_ignore("codecov.yml")
 usethis::use_build_ignore(".travis.yml")
 usethis::use_build_ignore("_config.yml")
 usethis::use_build_ignore("cran-comments.md")
@@ -16,7 +18,7 @@ devtools::check()
 devtools::check_man()
 devtools::missing_s3()
 devtools::release_checks()
-devtools::check_win_devel()
+# devtools::check_win_devel()
 
 # usethis::use_revdep()
 # devtools::revdep()
@@ -30,13 +32,11 @@ t2 <- Sys.time(); t2-t1
 
 devtools::build_manual()
 covr::codecov(token = "5ac8c633-0916-4cdc-9b1f-7391b8dafe7f")
+
+library("hexSticker")
+library(showtext)
+font_add_google("Russo One", "f")
+sticker("inst/figures/Rplot.png", package="spsurv", p_size=20, s_x=1, s_y=.75, s_width=.6,
+        h_fill="#01ACB6", h_color="#9be203",
+        filename="inst/figures/imgfile.png", p_family = "f")
 devtools::submit_cran()
-#install.packages("crossSurv_0.0.0.9000.tar.gz", repos = FALSE, dependencies=TRUE)
-# para eliminar o sequinte erro: Found the following hidden files and directories: .travis.yml
-# git add .travis.yml .Rbuildignore
-# git commit -m 'enable continuous integration via craigcitro/r-travis'
-# git push
-
-
-# for citation:
-# citation(auto = packageDescription("YPPE"))
